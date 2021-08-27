d0ce880251082ec736fc6346289f93f0f18e08a9
Jake Long <jakeprogrammer@gmail.com>
2021-08-27 16:17:42 -0700

    Cleanup

--- octoprint_TemperatureLimiter/__init__.py
+++ octoprint_TemperatureLimiter/__init__.py
@@ -1,38 +1,16 @@
 # coding=utf-8
 from __future__ import absolute_import
-
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
+import time
 
 import octoprint.plugin
+from octoprint.util import RepeatedTimer
 
-class TemperaturelimiterPlugin(octoprint.plugin.SettingsPlugin,
-    octoprint.plugin.AssetPlugin,
-    octoprint.plugin.TemplatePlugin
-):
+class TemperaturelimiterPlugin(octoprint.plugin.StartupPlugin):
 
-    ##~~ SettingsPlugin mixin
+    ##~~ StartupPlugin hooks
 
-    def get_settings_defaults(self):
-        return {
-            # put your plugin's default settings here
-        }
-
-    ##~~ AssetPlugin mixin
-
-    def get_assets(self):
-        # Define your plugin's asset files to automatically include in the
-        # core UI here.
-        return {
-            "js": ["js/TemperatureLimiter.js"],
-            "css": ["css/TemperatureLimiter.css"],
-            "less": ["less/TemperatureLimiter.less"]
-        }
+    def on_after_startup(self):
+        self._logger.info(u"Starting up Temperature Limiter")
 
     ##~~ Softwareupdate hook
 
@@ -56,18 +34,8 @@ class TemperaturelimiterPlugin(octoprint.plugin.SettingsPlugin,
             }
         }
 
-
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Temperature Limiter Plugin"
-
-# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
-# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
-# compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
 __plugin_pythoncompat__ = ">=3,<4" # only python 3
-#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 def __plugin_load__():
     global __plugin_implementation__
